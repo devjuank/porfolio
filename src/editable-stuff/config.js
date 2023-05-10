d7fed2cdca5c277af2887b78eb4f9f6e2ee893d9
juan carlos ojeda gomez <juank@MacBookPro.local>
2023-05-10 22:59:52 +0100

    updated picture

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -33,7 +33,7 @@ const mainBody = {
 //b) a link to an hosted image
 //      i.e:profilePictureLink:"www.picturesonline.com/johnDoeFancyAvatar.jpg",
 //c) image in "editable-stuff" directory and use require("") to import here,
-//      i.e: profilePictureLink: require("../editable-stuff/juank.png"),
+//      i.e: profilePictureLink: require("../editable-stuff/juank.jpeg"),
 //d) If you do not want any picture to be displayed, just leave it empty :)
 //      i.e: profilePictureLink: "",
 // For Resume either provide link to your resume or import from "editable-stuff" directory
@@ -43,7 +43,7 @@ const mainBody = {
 const about = {
   show: true,
   heading: "About Me",
-  imageLink: require("../editable-stuff/juank.png"),
+  imageLink: require("../editable-stuff/juank.jpeg"),
   imageSize: 390,
   message:
     "My name is Juan Carlos. hello! my name is juan carlos, I have 7 years of experience working in IT, I started working as a full stack developer, then I specialized in backend, and finally working as SRE/Devops.",
@@ -71,12 +71,12 @@ const leadership = {
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu bibendum at varius. Libero justo laoreet sit amet cursus sit amet. Imperdiet dui accumsan sit amet nulla facilisi morbi. At auctor urna nunc id. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Et magnis dis parturient montes nascetur ridiculus mus mauris. In nisl nisi scelerisque eu ultrices vitae auctor. Mattis nunc sed blandit libero volutpat sed cras ornare. Pulvinar neque laoreet suspendisse interdum consectetur libero.",
   images: [
     { 
-      img: require("../editable-stuff/juank.png"), 
+      img: require("../editable-stuff/juank.jpeg"), 
       label: "First slide label", 
       paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
     },
     { 
-      img: require("../editable-stuff/juank.png"), 
+      img: require("../editable-stuff/juank.jpeg"), 
       label: "Second slide label", 
       paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
     },
