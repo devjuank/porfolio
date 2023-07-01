c57dd08186479e0b343e65beb4905acf76232b9d
Juan Carlos Ojeda Gomez <juanki.ijcog@gmail.com>
2023-07-01 13:53:17 +0100

    added test to pipeline

--- src/hooks/useResizeObserver.js
+++ src/hooks/useResizeObserver.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ResizeObserver from 'resize-observer-polyfill';
 
 const useResizeObserver = (ref) => {
   const [dimensions, setDimensions] = React.useState(null);
